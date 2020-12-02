692e0b2b9a109ac9aebee1de9803d1b0f591cfe7
Juandi <juandibp@gmail.com>
2020-12-01 23:36:01 -0600

    base class

---
+++ MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/MainMemory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itcr.memorypagingsimulator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juand
+ */
+public class MainMemory {
+    private int maxFrames;
+    private ArrayList<Frame> frameList = new ArrayList<>();
+}
