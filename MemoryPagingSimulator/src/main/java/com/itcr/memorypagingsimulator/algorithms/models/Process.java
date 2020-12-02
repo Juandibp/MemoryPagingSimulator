a6b95cfdd78b6c72b66660cae89619891883bafc
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-12-02 01:51:24 -0600

    Filled process with hapiness

--- MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Process.java
+++ MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Process.java
@@ -5,10 +5,38 @@
  */
 package com.itcr.memorypagingsimulator.algorithms.models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lopez
  */
 public class Process {
+    
+    private int id;
+    private ArrayList<Page> pageTable = new ArrayList<>();
+    
+    //al proceso le toca traducir la direccion... 
+    //el proceso maneja internamente ids de pagina segun el indice en su tabla
+
+    public Process(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Page> getPageTable() {
+        return pageTable;
+    }
+
+    public void setPageTable(ArrayList<Page> pageTable) {
+        this.pageTable = pageTable;
+    }
     
 }
