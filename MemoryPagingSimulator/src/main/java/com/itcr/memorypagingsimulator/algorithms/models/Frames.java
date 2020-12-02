d4cca0f329ffef7cf44d2c8c2fe6dce6ddfd67e9
Juandi <juandibp@gmail.com>
2020-12-02 00:27:32 -0600

    me cago en netbeans

--- MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Frames.java
+++ MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Frames.java
@@ -5,10 +5,67 @@
  */
 package com.itcr.memorypagingsimulator.algorithms.models;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author juand
  */
-public class Frames {
+public class Frames implements Iterable<Integer>{
+    private List<Integer> frames;
     
+    public Frames(int numberOfFrames) {
+		this.frames = new ArrayList<>(numberOfFrames);
+		for (int i = 0; i < numberOfFrames; i++) {
+			frames.add(-1);
+		}
+	}
+    
+    public void deallocateFrames() {
+		int len = frames.size();
+		for (int i = 0; i < len; i++) {
+			frames.set(i, -1);
+		}
+	}
+    
+    public boolean contains(int reference) {
+		return indexOf(reference) > -1;
+	}
+
+	public int indexOf(int reference) {
+		return frames.indexOf(reference);
+	}
+
+	public int set(int index, int reference) {
+		return frames.set(index, reference);
+	}
+
+	public void swap(int victim, int reference) {
+		set(indexOf(victim), reference);
+	}
+
+	public int get(int index) {
+		return frames.get(index);
+	}
+
+	public boolean thereIsAnEmptyFrame() {
+		return contains(-1);
+	}
+
+	public void copyAll(Frames f) {
+		frames.clear();
+		frames = new ArrayList<>(f.frames);
+	}
+
+	public int getEmptyFrame() {
+		return indexOf(-1);
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		return frames.iterator();
+	}
+        
 }
