98593337b99b14c639f96a12e1d8d1234c83ab4e
Juandi <juandibp@gmail.com>
2020-12-07 14:23:54 -0600

    Modified Frames to acommodate a list of pages

--- MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Frames.java
+++ MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/models/Frames.java
@@ -14,22 +14,22 @@ import java.util.List;
  * 
  * @author juand
  */
-public class Frames implements Iterable<Integer>{
+public class Frames implements Iterable<Page>{
     
     /** Los numeros son los ids de las paginas */
-    private List<Integer> frames;
+    private List<Page> frames;
     
     public Frames(int numberOfFrames) {
         this.frames = new ArrayList<>(numberOfFrames);
         for (int i = 0; i < numberOfFrames; i++) {
-                frames.add(-1);
+                frames.add(null);
         }
     }
     
     public void deallocateFrames() {
         int len = frames.size();
         for (int i = 0; i < len; i++) {
-                frames.set(i, -1);
+                frames.set(i, null);
         }
     }
     
@@ -40,19 +40,19 @@ public class Frames implements Iterable<Integer>{
     public int indexOf(int reference) {
         return frames.indexOf(reference);
     }
-
-    public int set(int index, int reference) {
+    /*
+    public int set(int index, Page reference) {
         return frames.set(index, reference);
     }
 
     public void swap(int victim, int reference) {
         set(indexOf(victim), reference);
-    }
+    }*/
 
-    public int get(int index) {
+    /*public int get(int index) {
         return frames.get(index);
     }
-
+*/
     public boolean thereIsAnEmptyFrame() {
         return contains(-1);
     }
@@ -67,11 +67,11 @@ public class Frames implements Iterable<Integer>{
     }
 
     @Override
-    public Iterator<Integer> iterator() {
+    public Iterator<Page> iterator() {
         return frames.iterator();
     }
 
-    public List<Integer> getFrames() {
+    public List<Page> getFrames() {
         return frames;
     }
     
