68b24ce8a2b0ec67c6777fef70a8bab2c83401e3
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-12-09 14:22:25 -0600

    Create DemandCleaning.java

---
+++ MemoryPagingSimulator/src/main/java/com/itcr/memorypagingsimulator/algorithms/DemandCleaning.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itcr.memorypagingsimulator.algorithms;
+
+import com.itcr.memorypagingsimulator.algorithms.models.Frames;
+import com.itcr.memorypagingsimulator.algorithms.models.Page;
+import com.itcr.memorypagingsimulator.algorithms.models.Pages;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lopez
+ */
+public class DemandCleaning extends CleaningPolicy {
+
+    @Override
+    public void clean(ArrayList<Page> replacementPages, Frames frames, Pages pages) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
